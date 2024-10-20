6329aface18672d9313faae11a1e336584cdad3b
smelikeaslan <aslansumeyramelike@gmail.com>
2024-10-20 23:38:42 +0300

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Birinci sayyıyı giriniz:");
+
+double birincisayi = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("İkinci sayıyı giriniz:");
+
+double ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+
+double bolum = birincisayi / ikincisayi;
+
+
+Console.Write("Birinci sayının, ikinci sayıya bölümü:" + bolum);
+
+Console.ReadKey();
